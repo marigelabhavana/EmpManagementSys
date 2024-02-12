7f04086be3c46fcb288dcef2546ee7ff460e174f
Marigela Bhavana <marigelabhavana2000@gmail.com>
2024-02-12 16:34:59 +0530

    Add project files.

---
+++ EmpManagementSys/DAL/EmpProfile.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmpManagementSys.DAL
+{
+    [Table("EmpProfile")]
+    public class EmpProfile
+    {
+        [Key]
+        public int EmpCode { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? EmpName { get; set; }
+        public string? Email { get; set; }
+
+        [ForeignKey("DeptCode")]
+        public int DeptCode { get; set; }
+    
+    }
+}
---
+++ EmpManagementSys/Data/EMSContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmpManagementSys.DAL;
+
+namespace EmpManagementSys.Data
+{
+    public class EMSContext : DbContext
+    {
+        public EMSContext (DbContextOptions<EMSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmpManagementSys.DAL.EmpProfile> EmpProfile { get; set; } = default!;
+    }
+}
